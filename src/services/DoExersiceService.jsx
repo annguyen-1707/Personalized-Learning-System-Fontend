f1abe1e9ee2dbb59f3d2dadd48ce5e9a19cedd4f
Nguyen Trong Dat <datnthe180348@fpt.edu.vn>
2025-07-20 17:31:43 +0700

    Remove console log statements from CourseCard and DoExercise components to clean up code.

--- src/services/DoExersiceService.jsx
+++ src/services/DoExersiceService.jsx
@@ -1,38 +1,27 @@
 import axios from "../services/customixe-axios";
 const DoExerciseService = {
-  // doExercise: async (exerciseId, userId) => {
+
+  // getExerciseDetails: async (exerciseId) => {
   //   try {
-  //     const response = await axios.post(`/api/exercises/${exerciseId}/do`, {
-  //       userId,
-  //     });
+  //     const response = await axios.get(`/api/exercises/${exerciseId}`);
   //     return response.data;
   //   } catch (error) {
-  //     console.error("Error starting exercise:", error);
+  //     console.error("Error fetching exercise details:", error);
   //     throw error;
   //   }
   // },
 
-  getExerciseDetails: async (exerciseId) => {
-    try {
-      const response = await axios.get(`/api/exercises/${exerciseId}`);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching exercise details:", error);
-      throw error;
-    }
-  },
-
-  getExerciseResults: async (exerciseId, userId) => {
-    try {
-      const response = await axios.get(`/api/exercises/${exerciseId}/results`, {
-        params: { userId },
-      });
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching exercise results:", error);
-      throw error;
-    }
-  },
+  // getExerciseResults: async (exerciseId, userId) => {
+  //   try {
+  //     const response = await axios.get(`/api/exercises/${exerciseId}/results`, {
+  //       params: { userId },
+  //     });
+  //     return response.data;
+  //   } catch (error) {
+  //     console.error("Error fetching exercise results:", error);
+  //     throw error;
+  //   }
+  // },
 
   getSource: async (exerciseId) => {
     try {
